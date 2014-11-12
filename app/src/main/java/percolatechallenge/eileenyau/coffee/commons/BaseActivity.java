a9a79eed3a67dd702e742802fae415532742114b
Eileen <yau.eln@gmail.com>
2014-11-11 19:27:00 -0500

    additional comments

--- app/src/main/java/percolatechallenge/eileenyau/coffee/commons/BaseActivity.java
+++ app/src/main/java/percolatechallenge/eileenyau/coffee/commons/BaseActivity.java
@@ -11,30 +11,30 @@ public class BaseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(TAG, "OnCreate");
+        Log.v(TAG, "----------> OnCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.v(TAG, "onStart");
+        Log.v(TAG, "----------> onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v(TAG, "onResume");
+        Log.v(TAG, "----------> onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.v(TAG, "onPause");
+        Log.v(TAG, "----------> onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.v(TAG, "onStop");
+        Log.v(TAG, "----------> onStop");
     }
 }
