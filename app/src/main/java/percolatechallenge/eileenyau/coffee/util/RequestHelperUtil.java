3790e5db4e496d145a6ec05983e2b0e0a8e1499e
Eileen Yau <yau.eln@gmail.com>
2014-11-19 00:12:19 -0500

    Addd request helper util

--- app/src/main/java/percolatechallenge/eileenyau/coffee/util/RequestHelperUtil.java
+++ app/src/main/java/percolatechallenge/eileenyau/coffee/util/RequestHelperUtil.java
@@ -5,4 +5,13 @@ public class RequestHelperUtil {
     public static String buildUrlWithParams() {
         return null;
     }
+
+    public static boolean isJsonArray(String obj) {
+        boolean isJsonArray = false;
+
+        if (obj.length() > 0 && obj.charAt(0) == '[') {
+            isJsonArray = true;
+        }
+        return isJsonArray;
+    }
 }
